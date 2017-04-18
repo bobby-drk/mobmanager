23329c5d2efddb55442b7f264b7d264f4ca85293
Charlie King <charlie.king@clearlink.com>
2017-04-17 23:53:17 -0600

    Vue set up and basic layout

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,9 +1,11 @@
 
 window._ = require('lodash');
-require('bootstrap-sass');
+require('bulma');
 window.Vue = require('vue');
 
 window.axios = require('axios');
+window.moment = require('moment');
+moment().format();
 
 window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
---
+++ resources/assets/js/components/AdminPanel.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        admin
+
+        <p class="control">
+            <button class="button is-primary" @click="hideAdmin">Close</button>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+
+            };
+        },
+        methods: {
+            hideAdmin () {
+                this.$emit('closeAdmin')
+            }
+        },
+    }
+</script>
