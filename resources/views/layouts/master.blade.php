9329e9996bd49951eeb76863e076e2b77eb2afc2
Charlie King <charlie.king@clearlink.com>
2017-06-22 08:18:18 -0600

    Fixed extract by fixing file extension, upgraded Mix, using new autoload

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -31,6 +31,8 @@
             </div>
         </div>
 
+        <script type="text/javascript" src="{{ mix('/js/manifest.js') }}"></script>
+        <script type="text/javascript" src="{{ mix('/js/vendor.js') }}"></script>
         <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
 
         @stack('page-js')
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,5 +4,12 @@ mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .copyDirectory('resources/assets/sounds', 'public/sounds')
     .copyDirectory('resources/assets/images', 'public/images')
-    // .extract(['vue', 'vuex', 'jquery', 'bootstrap-sass', 'moment'])
+    .extract(['vue', 'vuex', 'bootstrap-sass', 'moment', 'lodash', 'jquery'])
+    .autoload({
+         jquery: ['$', 'jQuery', 'window.jQuery'],
+         lodash: ['window._'],
+         vue: ['window.Vue', 'Vue'],
+         axios: ['window.axios', 'axios'],
+         moment: ['window.moment', 'moment']
+    })
     .version();
