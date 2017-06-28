b53e0c7e2f24921dfbd54c89655abd28472cb1b2
Charlie King <charlie.king@clearlink.com>
2017-06-28 12:08:05 -0600

    sync test

--- tests/ScriptTests/participantsTest.js
+++ tests/ScriptTests/participantsTest.js
@@ -1,7 +1,8 @@
 import test from 'ava'
 import Vue from 'vue/dist/vue.js'
-import { participantAdd } from '../../resources/assets/js/store/mutations'
-import { participantsSync } from '../../resources/assets/js/store/mutations'
+import * as mutations  from '../../resources/assets/js/store/mutations'
+// import { participantsSync } from '../../resources/assets/js/store/mutations'
+// import from './mutations'
 
 
 
@@ -11,9 +12,9 @@ test('that a participant can be added', t => {
     const state = { participants: [] }
 
     //act
-    participantAdd(state,
+    mutations.participantAdd(state,
         {
-            name: 'Charlie',
+            name: 'Clark',
             contributor: true,
             active: false
         }
@@ -24,22 +25,49 @@ test('that a participant can be added', t => {
 
 })
 
-
 test('participant object can be synced', t => {
 
     // //arrange
-    // const state = { participants: [] }
+    const state = { participants: [] }
+    let participantObject = [
+        { name: 'Clark', contributor: true, active: true  },
+        { name: 'Bruce', contributor: true, active: false }
+    ]
 
     // //act
-    // participantAdd(state,
-    //     {
-    //         name: 'Charlie',
-    //         contributor: true,
-    //         active: false
-    //     }
-    // )
+    mutations.participantsSync(state, participantObject)
 
     // //assert
-    // t.is(state.participants.length, 1)
+    t.deepEqual(state.participants, participantObject)
+
+})
+
+
+test('a participant contributor can be toggled', t => {
+
+    // //arrange
+    const state = { participants: [
+        {
+            name: 'Clark',
+            contributor: true,
+            active: false
+        }
+    ] }
+
+
+    // //act
+    mutations.participantsSetContributor(state,         {
+            name: 'Clark',
+            contributor: true,
+            active: false
+    })
+console.log(state);
+
+    // //assert
+    t.deepEqual(state.participants, {
+            name: 'Clark',
+            contributor: false, // Now false
+            active: false
+    })
 
 });
