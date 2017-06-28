3c38e88207beb635cf74bb6308bedeb4ed5a7f78
Charlie King <charlie.king@clearlink.com>
2017-06-27 20:43:30 -0600

    Allow double click to edit.

--- resources/assets/js/components/FrontPanel.vue
+++ resources/assets/js/components/FrontPanel.vue
@@ -2,7 +2,35 @@
     <div class='mob-board'>
         <div class='row bottom-buffer '>
             <div class="col-md-11">
-                <h2 class="margin-manipulator-m" v-if="mobName || featureName">{{ mobName }} <span v-if="featureName"> <span v-if="mobName">:</span> {{ featureName }}</span></h2>
+                <h2 class="margin-manipulator-m" v-if="mobName || featureName">
+                    <span v-if="!editMobName" @dblclick="editMobName = true">{{ mobName }} </span>
+
+                    <input type="text"
+                        class="header-edit"
+                        v-focus
+                        v-else
+                        v-model="mobName"
+                        @blur="editMobName = false"
+                        @keyup.enter="editMobName = false"
+                        @keyup.esc="editMobName = false" />
+
+                    <span v-if="mobName && featureName">:</span>
+
+                    <span v-if="featureName" @dblclick="editFeatureName = true">
+                        <span v-if="!editFeatureName">{{ featureName }}</span>
+
+
+                        <input type="text"
+                            class="header-edit"
+                            v-focus
+                            v-else
+                            v-model="featureName"
+                            @blur="editFeatureName = false"
+                            @keyup.enter="editFeatureName = false"
+                            @keyup.esc="editFeatureName = false" />
+
+                    </span>
+                </h2>
             </div>
             <div class="col-md-1">
                 <span class="glyphicon glyphicon-cog pull-right btn-admin" @click="adminOn"></span>
@@ -30,16 +58,33 @@
     import { mapMutations, mapState } from 'vuex'
 
     export default {
+        data () {
+            return {
+                editFeatureName: false,
+                editMobName: false
+            }
+        },
         computed: {
-            ...mapState({
-                mobName: state => state.mobName,
-                featureName: state => state.featureName,
-            }),
+            mobName: {
+                get() { return this.$store.state.mobName},
+                set(value) { this.$store.commit('setMobName', value) }
+            },
+            featureName: {
+                get() { return this.$store.state.featureName},
+                set(value) { this.$store.commit('setFeatureName', value) }
+            },
         },
         methods: {
             ...mapMutations({
                 adminOn : 'setAdminDisplayOn',
-            })
+            }),
+        },
+        directives: {
+            focus: {
+                inserted(el) {
+                  el.focus()
+                }
+            }
         }
     }
 </script>
